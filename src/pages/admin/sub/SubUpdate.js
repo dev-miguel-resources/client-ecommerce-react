3d59d2c9096e56159d154d3e6f19b4e968f2c9da
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-08-26 23:37:53 -0400

    SubUpdate complete

--- src/pages/admin/sub/SubUpdate.js
+++ src/pages/admin/sub/SubUpdate.js
@@ -28,9 +28,62 @@ const SubUpdate = ({ history, match }) => {
       setParent(s.data.parent);
     });
 
-    return (
-        <p>Still Working</p>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    updateSub(match.params.slug, { name, parent }, user.token)
+      .then((res) => {
+        setLoading(false);
+        setName("");
+        toast.success(`"${res.data.name}" is updated`);
+        history.push("/admin/sub");
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+        if (err.response.status === 400) toast.error(err.response.data);
+      });
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <AdminNav />
+        </div>
+        <div className="col">
+          {loading ? (
+            <h4 className="text-danger">Loading...</h4>
+          ) : (
+            <h4>Update sub category</h4>
+          )}
+
+          <div className="form-group">
+            <label>Parent category</label>
+            <select
+              name="category"
+              className="form-control"
+              onChange={(e) => setParent(e.target.value)}
+            >
+              <option>Please select</option>
+              {categories.length > 0 &&
+                categories.map((c) => (
+                  <option key={c._id} value={c._id} selected={c._id === parent}>
+                    {c.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+
+          <CategoryForm
+            handleSubmit={handleSubmit}
+            name={name}
+            setName={setName}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SubUpdate;
